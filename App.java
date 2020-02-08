3c79734ed7042aae7c0efb96a49c10d2081e87e3
Artiom U <arti.urb@gmail.com>
2020-02-08 14:56:05 +0100

    //gra v2

--- App.java
+++ App.java
@@ -1,11 +1,8 @@
 package prorject.diabolo;
-
 import prorject.diabolo.npc.CreatePlayer;
 import prorject.diabolo.npc.Monster;
 import prorject.diabolo.npc.Player;
-
 import java.util.List;
-
 
 public class App {
     public static void main(String[] args) {
@@ -13,22 +10,21 @@ public class App {
         Player player = CreatePlayer.createPlayer();
         List<Monster> monsters = MonsterCreationUtil.createMonsters();
 
-        monsters.get(0).receiveDamage(player.attack());
-        monsters.stream().filter(m -> m.isAlive())
-                .forEach(singleMonster -> player.receiveDamage(singleMonster.attack()));
-        System.out.println(monsters);
-        while (player.isAlive() && monsters.stream().anyMatch(m -> m.isAlive())) {
+        while (player.isAlive()
+                && monsters.stream().anyMatch(m -> m.isAlive())) {
 
             monsters.stream()
                     .filter(m -> m.isAlive())
                     .findFirst()
                     .get()
                     .receiveDamage(player.attack());
-
-
             monsters.stream().filter(m -> m.isAlive())
                     .forEach(singleMonster -> player.receiveDamage(singleMonster.attack()));
-            System.out.println(monsters);
+        }
+        if (player.isAlive()) {
+            System.out.println("CONGRATULATIONS! You have killed " + monsters.size());
+        } else {
+            System.out.println("You died...");
         }
     }
 }
